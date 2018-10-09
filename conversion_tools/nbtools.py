c55a919ab1511313a4471fd9a726fe7eaf23f56d
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-10-09 06:43:22 -0700

    added a function to nbtools.py that runs notebooks and saves them as the same filename

--- conversion_tools/nbtools.py
+++ conversion_tools/nbtools.py
@@ -6,9 +6,11 @@ save jupyter notebooks
 do other things needed with jupyter notebooks
 """
 
+import os
 from pathlib import Path
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
+
 
 def nbrun(notebook_Path):
     """
@@ -20,16 +22,54 @@ def nbrun(notebook_Path):
     # need to do some sort of Path validation
     # need to do some sort of does it have an.ipynb extension check
     # need to do some sort of yes it is a valid notebook check
-    #if not Path(notebook_Path).exists():
-        #print(f'Notebook Path {notebook_Path} does not exist or is not a valid path')
-        #return
+    # if not Path(notebook_Path).exists():
+    # print(f'Notebook Path {notebook_Path} does not exist or is not a valid path')
+    # return
     if Path(notebook_Path).exists():
         print(f'Running notebook: {notebook_Path}')
         # run the notebook. See: https://nbconvert.readthedocs.io/en/latest/execute_api.html#executing-notebooks-using-the-python-api-interface
         with open(notebook_Path) as f:
             nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python3') #configure the notebook executor
-        ep.preprocess(nb, {'metadata': {'path': 'notebook_rundir/'}})      #run the notebook
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')  # configure the notebook executor
+        ep.preprocess(nb, {'metadata': {'path': 'notebook_rundir/'}})  # run the notebook
         with open('notebook_Path', 'wt') as f:
             nbformat.write(nb, f)
 
+
+def run_notebook(notebook_path):
+    """
+
+    A function to run every cell in a Jupyter notebook.
+    Opens, runs, and saves the Jupyter notebook. Notebook saved with the same filename.
+    Function will overwrite the notebook it runs.
+
+    Usage
+
+    from nbtools import run_notebook
+    run_notebook('Untitled.ipynb')
+
+    :param notebook_path: path to notebook, needs to be readable by os.path
+    :return: None, but a notebook (the one that is run) is saved to disk
+    """
+
+    nb_name = os.path.splitext(os.path.basename(notebook_path))
+    dirname = os.path.dirname(notebook_path)
+
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=600, kernal_name="python3")
+    proc.allow_errors = True
+
+    proc.preprocess(nb, {'metadata': {"path": '/'}})
+
+    with open(notebook_path, mode="wt") as f:
+        nbformat.write(nb, f)
+
+
+def main():
+    run_notebook('Untitled.ipynb')
+
+
+if __name__ == "__main__":
+    main()
